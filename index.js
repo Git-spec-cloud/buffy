561d3f89197965b5963f9b471bc96943c0e1159c
Git-spec-cloud <149431923+Git-spec-cloud@users.noreply.github.com>
2024-02-04 11:27:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(`
 `);
 
 const server = http.createServer();
-const url = "https://selenite.cc";
+const url = "https://now.gg/apps/roblox-corporation/5349/roblox.html";
 const PORT = process.env.PORT || 8080;
 
 server.on("request", async (req, res) => {
